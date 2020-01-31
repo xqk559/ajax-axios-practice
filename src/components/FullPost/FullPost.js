3411df672b6f3ca726c99b4b7c4274c03cd39f4d
keith <xqk559@gmail.com>
2020-01-31 10:03:36 -0500

    GET loop fixed

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -9,13 +9,16 @@ class FullPost extends Component {
 
     componentDidUpdate() {
         if (this.props.id) {
+            if ( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+                
+            
         axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
             .then(response => {
                 // console.log(response);
                 this.setState({loadedPost: response.data});
             });
         }
-    }
+    }}
 
     render () {
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
